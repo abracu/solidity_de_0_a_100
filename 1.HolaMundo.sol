abfa512edc30d06a6a99c98465e041ea322131fe
Alfredo Bravo Cuero <yo@alfredobravocuero.co>
2023-07-18 13:26:51 +0200

    Hola Mundo del dia 1 de #SolidityDe0a100

---
+++ 1.HolaMundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+// La versión del compilador debe ser mayor o igual que 0.8.17 y menor que 0.9.0
+
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    string public greet = "Hola Mundo";
+}
